ea43d81aaf69a6331b3799d8f94a0c69dafd6b97
Zahra Seyedi <za.seyedi@gmail.com>
2021-04-19 20:26:56 -0700

    Display word definition loop AND Display Giphy

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ var giphyKey = "jCojN0t1UlrJ7c9lCdhqS3UMkGvT9924";
 
 var definition = () => {
   var txt = $("#entryField").val();
-   // var txt = txt;
   var apiURL = "https://www.dictionaryapi.com/api/v3/references/collegiate/json/" + txt +"?key=" + defKey;
     fetch (apiURL)
     .then((Response) => {
@@ -16,24 +15,27 @@ var definition = () => {
         })
 };
 
+
 // Display word definition
-displayDefinition = (data) => {
-  this.data = data;
-  var defHTML = `<p>${data[0].shortdef[0]}</p>`;
-  $("#definitionBox").html(defHTML);
+var displayDefinition = (data) => {
+  console.log(data);
+  $.each(data[0].shortdef, function(i, value){
+    console.log(value);
+    $("#definitionBox").append(value + '<br>')
+  })
 }
+ 
 
 
 // Display Giphy 
 var giphy = () => {
   var giphyTXT = $("#entryField").val();
-  var giphyURL = "https://api.giphy.com/v1/gifs/trending?api_key="+ giphyKey + "&q=" + giphyTXT + "&limit=25&offset=0&rating=g&lang=en";
+  var giphyURL = "https://api.giphy.com/v1/gifs/search?api_key="+ giphyKey + "&q=" + giphyTXT + "&limit=25&offset=0&rating=g&lang=en";
   fetch (giphyURL)
   .then((Response) => {
     return Response.json();
   })
   .then((json) => {
-    //console.log(json.data[0].images.fixed_height.url);
     var src = json.data[0].images.fixed_height.url;
     var gifHTML = $(`<img/>`).attr('src', src);
     $("#gifBox").html(gifHTML);
