8d0e786241ef9065b7d8088f1b761b9c119ece25
ericrudeen <78781277+ericrudeen@users.noreply.github.com>
2021-04-21 19:52:52 -0700

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,10 @@
 var defKey = "f3c52c90-008b-4455-b175-2d43e6e996af";
 var giphyKey = "jCojN0t1UlrJ7c9lCdhqS3UMkGvT9924";
+var definitionBox = document.getElementById("definitionBox"); 
 
 
 var definition = () => {
   var txt = $("#entryField").val();
-   // var txt = txt;
   var apiURL = "https://www.dictionaryapi.com/api/v3/references/collegiate/json/" + txt +"?key=" + defKey;
     fetch (apiURL)
     .then((Response) => {
@@ -16,12 +16,17 @@ var definition = () => {
         })
 };
 
+
 // Display word definition
-displayDefinition = (data) => {
-  this.data = data;
-  var defHTML = `<p>${data[0].shortdef[0]}</p>`;
-  $("#definitionBox").html(defHTML);
-}
+var displayDefinition = (data) => {
+  
+  $.each(data[0].shortdef, (i, value) => {
+    
+    $("#definitionBox").append(`<li>` +value + '<br>')
+  })
+
+};
+ 
 
 
 // Display Giphy 
@@ -33,12 +38,11 @@ var giphy = () => {
     return Response.json();
   })
   .then((json) => {
-    //console.log(json.data[0].images.fixed_height.url);
     var src = json.data[0].images.fixed_height.url;
     var gifHTML = $(`<img/>`).attr('src', src);
     $("#gifBox").html(gifHTML);
-  })
-}
+  });
+};
 
 
 
@@ -46,13 +50,7 @@ var giphy = () => {
 // Word search listener
 $("#searchBtn").on("click",(event) => {
   event.preventDefault();
+  definitionBox.innerHTML = "";
   definition(event)
   giphy(event);
 });
-
-
-
-
-
-
-
